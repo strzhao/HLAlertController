bafff9959e7bcf0ea3240463bbcc0fe81ad96eb1
JustString <497492762@qq.com>
2015-05-18 22:04:27 +0800

    Initial Commit

---
+++ HLAlertController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HLAlertController
+//
+//  Created by String on 15/5/18.
+//  Copyright (c) 2015年 ___string___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HLAlertController/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HLAlertController
+//
+//  Created by String on 15/5/18.
+//  Copyright (c) 2015年 ___string___. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
