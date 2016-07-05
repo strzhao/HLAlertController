ec4f54581fdba9c265996ba2581c4bc3f237ed09
JustString <497492762@qq.com>
2016-07-05 14:51:23 +0800

    support cocoapods

---
+++ HLAlertController/HLAlertController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HLAlertController
+//
+//  Created by String on 16/7/5.
+//  Copyright © 2016年 ___string___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
